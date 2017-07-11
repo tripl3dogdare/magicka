9b270f5760ef9be4890f0037097f21d0fbefb5e1
tripl3dogdare <connor@connorjulian.org>
2017-07-10 18:00:30 -0600

    Expand mod skeleton to accomodate larger mod
    The mod skeleton from the previous commit was adapted from my previous mod Wrench in the Works, which fits into two files (3 for 1.10). Magicka, on the other hand, is going to be a much more expansive mod, so it's skeleton needed a bit of expansion to accomodate.
    
    I've also added the US localization file and main creative tab for later use.

--- src/main/scala/com/tripl3dogdare/magicka/Magicka.scala
+++ src/main/scala/com/tripl3dogdare/magicka/Magicka.scala
@@ -11,6 +11,9 @@ import net.minecraft.block.Block
 import net.minecraftforge.common.config.ConfigManager
 import org.apache.logging.log4j.LogManager
 import net.minecraftforge.fml.common.network.NetworkRegistry
+import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.item.ItemStack
+import net.minecraft.init.Enchantments
 
 @Mod(modid=Magicka.MODID, name=Magicka.NAME, version=Magicka.VERSION, modLanguage="scala")
 @Mod.EventBusSubscriber
@@ -19,16 +22,31 @@ object Magicka {
   final val NAME = "Magicka"
   final val VERSION = "Indev"
 
-  val console = LogManager.getLogger("magicka")
+  val console = LogManager.getLogger(MODID)
 	val network = NetworkRegistry.INSTANCE.newSimpleChannel(MODID)
 
-  @SubscribeEvent def registerItems(e:RegistryEvent.Register[Item]) = {
+  @SubscribeEvent def registerItems(e:RegistryEvent.Register[Item]) = Items.register(e)
+  @SubscribeEvent def registerBlocks(e:RegistryEvent.Register[Block]) = Blocks.register(e)
+  @SubscribeEvent def registerModels(e:ModelRegistryEvent) = Models.register(e)
+
+  object Items {
+    def register(e:RegistryEvent.Register[Item]) {}
   }
 
-  @SubscribeEvent def registerBlocks(e:RegistryEvent.Register[Block]) = {
+  object Blocks {
+    def register(e:RegistryEvent.Register[Block]) {}
   }
 
-  @SubscribeEvent def registerModels(e:ModelRegistryEvent) = {
+  object Models {
+    def register(e:ModelRegistryEvent) {}
+  }
+
+	val tabMain = new CreativeTabs("magicka") {
+    override def getTabIconItem = {
+      val stack = new ItemStack(net.minecraft.init.Items.ENCHANTED_BOOK)
+      stack.addEnchantment(Enchantments.SMITE, 0)
+      stack
+    }
   }
 
   @SubscribeEvent def onConfigChanged(e:OnConfigChangedEvent) =
